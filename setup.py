53ddeb76ea2337069bd2d4293c945d3b6fa0936b
Dean Gardiner <gardiner91@gmail.com>
2013-01-04 00:09:24 +1300

    Minor reformat of [setup.py]

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ from distutils.core import setup
 
 __author__ = 'Dean Gardiner'
 
-setup(name='PyUPnP',
-      version='0.8a',
-      description='Simple Python UPnP device discovery library',
-      author='Dean Gardiner',
-      author_email='gardiner91@gmail.com',
-      url='http://github.com/fuzeman/PyUPnP',
-      packages=['pyupnp'],
-      )
+setup(
+    name='PyUPnP',
+    version='0.8a',
+    description='Simple Python UPnP device discovery library',
+    author='Dean Gardiner',
+    author_email='gardiner91@gmail.com',
+    url='http://github.com/fuzeman/PyUPnP',
+    packages=['pyupnp'],
+)
